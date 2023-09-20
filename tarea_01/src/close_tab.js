0c90addea6bb284c8071639fe3e4250d047f771f
JMDH76 <jmdominguezhernandez@gmail.com>
2023-09-20 21:52:46 +0200

    CS_20230920_2152

--- tarea_01/src/close_tab.js
+++ tarea_01/src/close_tab.js
@@ -1,5 +1,9 @@
 function close_tab() {
+        window.close();
+}
+
+function close_tab2() {
     if (confirm("Do you want to close this tab?")) {
-      window.close();
+        window.close();
     }
-  }
+}
