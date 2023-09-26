da755cc72b63dcca8ff1e974f77dadac082175e5
JMDH76 <jmdominguezhernandez@gmail.com>
2023-09-26 20:39:07 +0200

    CS_20230926_2039

--- tarea_01/src/ejercicio_07.js
+++ tarea_01/src/ejercicio_07.js
@@ -1,24 +1,32 @@
 function question7() {
     let original = prompt("Indique una palabra o frase");
-    let sentence = original.toLowerCase();
-    isPalindrome(sentence, original);
+    isPalindrome(original);
 }
 
 
-function isPalindrome(sentence, original) {
+function isPalindrome(original) {
 
-    let arrayText = Array.from(sentence);
-    let arrayTextReverse = [];
-    let text = sentence.split(" ").join("");
-  
-    for (let i = 0; i < arrayText.length; i++) {
-        arrayTextReverse.unshift(arrayText[i]);
+    let vocals = ["a", "e", "i", "o", "u"];
+    let accentVocals = ["á", "é", "í", "ó", "ú"];
+
+    let sentence = original.toLowerCase().split(" ").join("");
+
+    //Quita acentos de las vocales
+    for (let i = 0; i < vocals.length; i++) {
+        sentence = sentence.replace(accentVocals[i], vocals[i]);
     }
+
+    let arrayTextReverse = [];
+    for (let i = 0; i < sentence.length; i++) {
+        arrayTextReverse.unshift(sentence.charAt(i));
+    }
+
     let textReverse = arrayTextReverse.toString();
     textReverse = textReverse.split(",").join("");
     textReverse = textReverse.split(" ").join("");
 
-    if (text === textReverse) {
+
+    if (sentence === textReverse) {
         alert(original + "\nEs palíndromo");
     } else {
         alert(original + "\nNo es palíndromo");
