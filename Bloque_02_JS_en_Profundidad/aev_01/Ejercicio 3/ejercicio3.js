69afe4018866e148b8394ef89328e6b3f83cea84
JMDH76 <jmdominguezhernandez@gmail.com>
2023-10-18 17:55:53 +0200

    AEV_01_Finalizada

--- Bloque_02_JS_en_Profundidad/aev_01/Ejercicio 3/ejercicio3.js
+++ Bloque_02_JS_en_Profundidad/aev_01/Ejercicio 3/ejercicio3.js
@@ -1,27 +1,13 @@
-//Cargamos las rutas de las imágenes en un array
-let contents = [
-    { title: 'Entrantes', path: "url(./src/entrantes/1.jpg)" },
-    { title: 'Postres', path: "url(./src/tartas/1.jpg)" },
-    { title: 'Bebidas', path: 'url(./src/alcohol/1.jpg)' }
-];
-
-/* let starters = ['./src/entrantes/1.jpg', './src/entrantes/2.jpg', './src/entrantes/3.jpg', './src/entrantes/4.jpg', './src/entrantes/5.jpg'];
-let alcohol = ['./src/alcohol/1.jpg', './src/alcohol/2.jpg', './src/alcohol/3.jpg', './src/alcohol/4.jpg', './src/alcohol/5.jpg']
-let cofffe = ['./src/cafe/1.jpg', './src/cafe/2.jpg', './src/cafe/3.jpg', './src/cafe/4.jpg', './src/cafe/5.jpg']
-let fruit = ['./src/fruit/1.jpg', './src/fruit/2.jpg', './src/fruit/3.jpg', './src/fruit/4.jpg', './src/fruit/5.jpg']
-let infusions = ['./src/infusiones/1.jpg', './src/infusiones/2.jpg', './src/infusiones/3.jpg', './src/infusiones/4.jpg', './src/infusiones/5.jpg']
- */
+let contents = ['Entrantes', 'Postres', 'Bebidas'];
 let galeryImages = [
-    { title: 'starters', url: './src/entrantes/' },
-    { title: 'alcohol', url: './src/alcohol/' },
-    { title: 'coffe', url: './src/cafe/' },
-    { title: 'fruit', url: './src/fruta/' },
-    { title: 'infusions', url: './src/infusiones/' }
+    { title: 'Entrantes', category: 'Entrantes', url: './src/entrantes/', path: 'url(./src/entrantes/1.jpg)' },
+    { title: 'Fruta', category: 'Postres', url: './src/fruta/', path: 'url(./src/fruta/1.jpg)' },
+    { title: 'Tartas', category: 'Postres', url: './src/tartas/', path: 'url(./src/tartas/1.jpg)' },
+    { title: 'Café', category: 'Bebidas', url: './src/cafe/', path: 'url(./src/cafe/1.jpg)' },
+    { title: 'Infusiones', category: 'Bebidas', url: './src/infusiones/', path: 'url(./src/infusiones/1.jpg)' },
+    { title: 'Alcohol', category: 'Bebidas', url: './src/alcohol/', path: 'url(./src/alcohol/1.jpg)' }
 ];
 
-
-
-//Obtenemos los elementos que vamos a usar
 let img = document.getElementsByTagName('img')[0];
 img.remove();
 let botonera = document.getElementsByTagName('tr')[0];
@@ -30,84 +16,139 @@ let next = document.getElementById('siguiente');
 let pageTitle = document.getElementsByTagName('h1')[0];
 let table = document.getElementsByTagName('td')[2];
 
+let tbody = document.getElementsByTagName('tbody')[0];
+let tr = document.createElement('tr');
+let td = document.createElement('td');
+let photo = document.createElement('img');
+
 let actual = 0;
 initialImageFormat(actual);
 
+
 //Botonera
 previus.addEventListener('click', () => {
+    deleteImage();
     if (actual === 0) {
         actual = contents.length - 1;
     } else {
         actual--;
     }
-
-    document.getElementsByTagName('h1')[1].remove();
     initialImageFormat(actual);
-    console.log(pageTitle.textContent);
 });
 
 next.addEventListener('click', () => {
+    deleteImage();
     if (actual === contents.length - 1) {
         actual = 0;
     } else {
         actual++;
     }
-
-    document.getElementsByTagName('h1')[1].remove();
     initialImageFormat(actual);
-    console.log(pageTitle.textContent);
 });
 
 
-
-//Vuelta atrás título
-pageTitle.addEventListener('click', () => {
-    for (let i = 0; i < galeryImages.length; i++) {
-        document.getElementById(`image${i}`).remove();
+//Borrado de imágenes
+function deleteImage() {
+    for (let i = 0; i <= actual; i++) {
+        document.getElementsByTagName('h1')[1].remove();
     }
-    initialImageFormat(actual);
-    //h1.style.display = 'block';
-});
+}
 
 
+//Imágenes del carrusel
+function initialImageFormat(pos) {
+    pageTitle.textContent = contents[pos];
+    let h1;
+    let index;
+    if (pos === 0) {
+        for (let i = 0; i < 1; i++) {
+            h1 = document.createElement('h1');
+            h1.style.backgroundImage = galeryImages[pos].path;
+            h1.textContent = galeryImages[pos].title;
+            index = i;
+            createImage(h1, index);
+        }
+    } else if (pos === 1) {
+        for (let i = 0; i < 2; i++) {
+            h1 = document.createElement('h1');
+            h1.style.backgroundImage = galeryImages[pos + i].path;
+            h1.textContent = galeryImages[pos + i].title;
+            index = i + 1;
+            createImage(h1, index);
+        }
+    } else if (pos === 2) {
+        for (let i = 0; i < 3; i++) {
+            h1 = document.createElement('h1');
+            h1.style.backgroundImage = galeryImages[pos + i + 1].path;
+            h1.textContent = galeryImages[pos + i + 1].title;
+            index = i + 3;
+            createImage(h1, index);
+        }
+    }
+}
+
+function createImage(h1, index) {
+    h1.style.backgroundSize = 'cover';
+    h1.style.backgroundPosition = 'center center';
+    h1.style.color = 'white';
+    h1.style.display = 'inline-block';
+    h1.style.width = '31%';
+    h1.style.height = '400px';
+    h1.style.margin = '0.5%';
+    h1.style.marginTop = "5%"
+    table.appendChild(h1);
+    openGalery(h1, index);
+}
 
 
+//Abrir galería pulsando sobre imagen
+function openGalery(h1, index) {
+    h1 = document.getElementsByTagName('h1');
+    for (let i = 1; i < h1.length; i++) {
+        h1[i].addEventListener('click', () => {
+            deleteImage();
+            generateGalery(index);
+        });
+    }
+}
 
-//genera galeria
-let h1 = document.getElementsByTagName('h1')[1];
-h1.addEventListener('click', () => {
-    galeryAccess();
-});
-
-
-
-
-
-function galeryAccess() {
-    h1.remove();
-    //h1.style.display = 'none';
-    //table.style.display = 'flex';
-
+function generateGalery(index) {
     for (let i = 0; i < 5; i++) {
         let galery = document.createElement('img');
         galery.setAttribute('class', 'galery');
-        galery.setAttribute('id', `image${i}`);
-        galery.style.width = '19%';
-        galery.style.marginLeft = '1%';
-        galery.src = galeryImages[actual].url + `${i + 1}.jpg`;
-        galery.alt = 'imagen';
+        galery.setAttribute('id', `image${i + 1}`);
+        galery.style.width = '18%';
+        galery.style.margin = '0.5%';
+        galery.style.marginTop = '5%';
+        galery.src = galeryImages[index].url + `${i + 1}.jpg`;
+        galery.alt = `imagen ${i + 1}`;
         table.appendChild(galery);
+        table.style.display = 'initial';
+        botonera.style.display = 'none'
+
+        galery.addEventListener('mousedown', () => {
+            tbody.appendChild(tr);
+            tr.appendChild(td);
+            td.appendChild(photo);
+            photo.src = galeryImages[index].url + `${i + 1}.jpg`;
+            galery.addEventListener('mouseup', () => {
+                tr.remove();
+            });
+        });
     }
 }
 
-function initialImageFormat(pos) {
-    let newH1 = document.createElement('h1');
-    newH1.style.backgroundImage = contents[pos].path;
-    newH1.innerHTML = contents[pos].title;
-    newH1.style.backgroundSize = 'cover';
-    newH1.style.backgroundPosition = 'center center';
-    newH1.style.height = '350px';
-    newH1.style.color = 'white';
-    pageTitle.innerHTML = contents[pos].title;
-    table.appendChild(newH1);
-}
+
+//Salir de galeria
+pageTitle.addEventListener('click', () => {
+    if (document.getElementsByTagName('img')[0] !== undefined) {
+        botonera.style.display = 'block'
+        for (let i = 0; i < 5; i++) {
+            document.getElementById(`image${i + 1}`).remove();
+        }
+        initialImageFormat(actual);
+    }
+});
+
+
+
