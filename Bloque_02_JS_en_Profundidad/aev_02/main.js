c9c48c2adc37a29289995a66c967c6d4f390ef0e
JMDH76 <jmdominguezhernandez@gmail.com>
2023-11-11 19:53:56 +0100

    CS AEV_02. Botón siguiente

--- Bloque_02_JS_en_Profundidad/aev_02/main.js
+++ Bloque_02_JS_en_Profundidad/aev_02/main.js
@@ -1,12 +1,14 @@
 
-const getData = async () => {
+const getData = async (page) => {
     try {
-        const response = await axios.get('https://rickandmortyapi.com/api/character/?page=1');
+        const response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`);
+
+        //document.getElementById('trigger').setAttribute('class', 'trigger');
+        let pageNumber = document.getElementById('number-page').textContent = page;
 
         buildCards(2, response);
         modalWindow(response);
         showMore(response);
-
 
     } catch (error) {
         console.error(error);
@@ -15,22 +17,19 @@ const getData = async () => {
 
 
 const showMore = (response) => {
-    let showMoreButton = document.getElementsByTagName('button')[0];
-    let cards = Array.from(document.getElementsByClassName('card'));
-    let cardClone = cards[0].cloneNode(true);
+    const showMoreButton = document.getElementsByTagName('button')[0];
+    const cards = Array.from(document.getElementsByClassName('card'));
 
+    showMoreButton.addEventListener('click', () => {
+        cards.map((element, index) => {
+            if (index > 0) {
+                element.remove();
+            }
+        })
 
-    console.log('cards > ' + cards.length);
-
-    showMoreButton.addEventListener('click', () => {   
-           cards.map((element, index) => {
-               if (index > 0) {
-                   element.remove();
-               } 
-            })
-
-        buildCards(19, response, cardClone);
+        buildCards(19, response);
         modalWindow(response);
+        //getData(2);
     })
 }
 
@@ -38,10 +37,11 @@ const showMore = (response) => {
 const modalWindow = (response) => {
     let modalButton = document.getElementsByClassName('trigger');
     let modalContainer = document.getElementsByClassName('modal');
-    //console.log('modal > ' + Array.from(modalButton).length)
+
     Array.from(modalButton).map((element, index) => {
-       
-        document.getElementsByClassName('trigger')[index].addEventListener('click', function () {
+
+        document.getElementsByClassName('trigger')[index].addEventListener('click', () => {
+
             modalContainer[0].classList.toggle('show-modal');
             let modalImage = document.getElementsByClassName('modal-content')[0];
             modalImage.style.backgroundImage = `url(${response.data.results[index].image})`;
@@ -50,6 +50,7 @@ const modalWindow = (response) => {
             let modalText = document.getElementsByTagName('h1')[1];
             modalText.textContent = response.data.results[index].name;
         });
+        
         document.getElementsByClassName('close-button')[0].addEventListener('click', function () {
             modalContainer[0].classList.toggle('show-modal');
         });
@@ -60,16 +61,14 @@ const modalWindow = (response) => {
 const buildCards = (num, response) => {
 
     const container = document.getElementsByClassName('grid-container')[0];
-    const card = document.getElementsByClassName('card')[0];
+    const cards = document.getElementsByClassName('card');
     document.getElementById('trigger').setAttribute('class', 'trigger');
 
     for (let i = 0; i < num; i++) {
-        let cardClone = card.cloneNode(true);
+        let cardClone = cards[0].cloneNode(true);
         container.appendChild(cardClone);
     }
 
-    let cards = document.getElementsByClassName('card');
-    //console.log('modal > ' + Array.from(cards).length)
     Array.from(cards).map((element, index) => {
         document.getElementsByClassName('item-0')[index].style.backgroundImage = `url(${response.data.results[index].image})`;
         document.getElementsByClassName('item-1')[index].textContent = response.data.results[index].gender;
@@ -79,4 +78,4 @@ const buildCards = (num, response) => {
     });
 }
 
-window.onload = getData();
+window.onload = getData(1);
