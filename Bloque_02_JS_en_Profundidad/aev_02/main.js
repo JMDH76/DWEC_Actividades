60364000e36fcbe0fcda4f90ca11e9b833489c58
JMDH76 <jmdominguezhernandez@gmail.com>
2023-11-12 11:19:41 +0100

    AEV_02 Botones operativos

--- Bloque_02_JS_en_Profundidad/aev_02/main.js
+++ Bloque_02_JS_en_Profundidad/aev_02/main.js
@@ -1,50 +1,79 @@
 const getData = async (page) => {
-    console.log('GetData');
     try {
         const response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`);
-        let pageNumber = document.getElementById('number-page').textContent = page;
+        document.getElementById('number-page').textContent = page;
+        const actualPage = parseInt(document.getElementById('number-page').textContent);
 
-        buildCards(3, response);
-        showMore(response);
+        buildCards(2, response);
+        showMore(response, actualPage);
 
     } catch (error) {
         console.error(error);
     }
 }
 
-
-const showMore = (response) => {
-    console.log('ShowMore');
+/* Botón 'Mostrar más */
+const showMore = (response, actualPage) => {
     const showMoreButton = document.getElementsByTagName('button')[0];
-
     showMoreButton.addEventListener('click', () => {
-        buildCards(20, response);
-        nextButtonClick(response);
-        //document.getElementsByTagName('button')[0].style.display = 'none';
-    })
-
-}
-
-
-const nextButtonClick = (response) => {
-    console.log('nextButtonClick');
-    const showMoreButton = document.getElementsByTagName('button')[0];
-    const buttonContiner = document.getElementById('render-more');
-    const nextButton = document.createElement('button');
-    nextButton.innerHTML = 'SIGUIENTE';
-    nextButton.setAttribute('id', 'siguiente');
-    buttonContiner.appendChild(nextButton);
-
-    nextButton.addEventListener('click', (event) => {
-        document.getElementsByTagName('button')[0].style.display = 'block';
-        document.getElementById('siguiente').remove();
-        getData(2);
+        showMoreButton.remove();
+        if (actualPage === 42) {
+            previousPageButtonClick(actualPage)
+            buildCards(5, response);
+        } else {
+            if ((actualPage - 1) === 0) nextPageButtonClick(actualPage);
+            else {
+                previousPageButtonClick(actualPage);
+                nextPageButtonClick(actualPage);
+            }
+            buildCards(19, response);
+        }
     })
 }
 
+/* Botón 'Anterior' */
+const previousPageButtonClick = (actualPage) => {
+    const buttonContainer = document.getElementById('render-more');
+    const previousPageButton = document.createElement('button');
+    previousPageButton.innerHTML = 'ANTERIOR';
+    previousPageButton.style.marginRight = '5px'
+    previousPageButton.setAttribute('id', 'anterior');
+    buttonContainer.appendChild(previousPageButton);
 
+    previousPageButton.addEventListener('click', (event) => {
+        const nextPageButton = document.getElementById('siguiente');
+        previousPageButton.remove();
+        if (nextPageButton) nextPageButton.remove();
+
+        const showMoreButton = document.createElement('button');
+        showMoreButton.innerHTML = 'MOSTRAR MÁS';
+        buttonContainer.appendChild(showMoreButton);
+        getData(actualPage - 1);
+    })
+}
+
+/* Botón 'Siguiente' */
+const nextPageButtonClick = (actualPage) => {
+    const buttonContainer = document.getElementById('render-more');
+    const nextPageButton = document.createElement('button');
+    nextPageButton.innerHTML = 'SIGUIENTE';
+    nextPageButton.setAttribute('id', 'siguiente');
+    buttonContainer.appendChild(nextPageButton);
+
+    nextPageButton.addEventListener('click', (event) => {
+        const previousPageButton = document.getElementById('anterior');
+        nextPageButton.remove();
+        if (previousPageButton) previousPageButton.remove();
+
+        const showMoreButton = document.createElement('button');
+        showMoreButton.innerHTML = 'MOSTRAR MÁS';
+        buttonContainer.appendChild(showMoreButton);
+        getData(actualPage + 1);
+    })
+}
+
+/* Crea el modal y le asigna los valores correspondientes */
 const modalWindow = (response) => {
-    console.log('Entro en modalWindow');
     const modalButtons = document.getElementsByClassName('trigger');
     const modalContainer = document.getElementsByClassName('modal')[0];
     const closeButton = document.getElementsByClassName('close-button')[0];
@@ -58,43 +87,32 @@ const modalWindow = (response) => {
             modalImage.style.backgroundSize = 'cover';
             modalText.textContent = response.data.results[index].name;
         });
-
         closeButton.addEventListener('click', function () {
             modalContainer.classList.remove('show-modal');
-            console.log('ok')
         });
     });
 }
 
-
+/* Crea los Cards necesarios */
 const buildCards = (newCards, response) => {
-    console.log('Entro en BuildCards');
-    console.log(response.data.results)
     const container = document.getElementsByClassName('grid-container')[0];
-    let cards = document.getElementsByClassName('card');
+    const cards = document.getElementsByClassName('card');
     document.getElementById('trigger').setAttribute('class', 'trigger');
-    
-    
-    console.log('Cartas > 1' + Array.from(cards).length)
 
+    /* Si existen borra todas menos la primera */
     Array.from(cards).map((element, index) => {
         if (index > 0) {
             element.remove();
         }
     })
 
-    console.log('Cartas2 > ' + Array.from(cards).length)
-    
+    /* Clona la primera y las añade */
     for (let i = 0; i < newCards; i++) {
         let cardClone = cards[0].cloneNode(true);
         container.appendChild(cardClone);
     }
-    console.log('Cartas3 > ' + Array.from(cards).length)
-    
-    cards[0].remove();
 
-    console.log('Cartas4 > ' + Array.from(cards).length)
-    
+    /* Da los valores correspondientes a cada Card */
     Array.from(cards).map((element, index) => {
         document.getElementsByClassName('item-0')[index].style.backgroundImage = `url(${response.data.results[index].image})`;
         document.getElementsByClassName('item-1')[index].textContent = response.data.results[index].gender;
@@ -102,7 +120,6 @@ const buildCards = (newCards, response) => {
         document.getElementsByClassName('item-3')[index].textContent = response.data.results[index].name;
         document.getElementsByClassName('item-4')[index].textContent = response.data.results[index].status;
     });
-
     modalWindow(response);
 }
 
