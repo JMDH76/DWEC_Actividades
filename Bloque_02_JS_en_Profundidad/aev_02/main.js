0ecc2ca1fe4ed359f9ba5a10850130885e969693
JMDH76 <jmdominguezhernandez@gmail.com>
2023-11-12 14:07:41 +0100

    CS AEV_02 Favoritos operativo

--- Bloque_02_JS_en_Profundidad/aev_02/main.js
+++ Bloque_02_JS_en_Profundidad/aev_02/main.js
@@ -1,15 +1,53 @@
 const getData = async (page) => {
     try {
-        const response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`);
+        let response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`);
         document.getElementById('number-page').textContent = page;
+        response = response.data.results;
+        console.log(response)
+        console.log(typeof (response));
         const actualPage = parseInt(document.getElementById('number-page').textContent);
 
-        buildCards(2, response);
+        buildCards(2, response, actualPage);
         showMore(response, actualPage);
-
+        favorites();
     } catch (error) {
         console.error(error);
     }
+}
+
+
+const favorites = (actualPage) => {
+    let response = [];
+    const title = document.getElementsByTagName('h1');
+
+    title[0].addEventListener('click', () => {
+
+        Array.from(localStorage).map((element, index) => {
+            let key = localStorage.key(index);
+            let item = localStorage.getItem(key);
+
+            item = JSON.parse(item);
+            response.push(item)
+
+        })
+        console.log(response)
+        buildCards(response.length-1, response, actualPage);
+
+    })
+
+}
+
+
+/* Obtener datos de localStorage */
+/* let getFavoritesData = (key) => {
+    let data = window.localStorage.getItem(key);
+    return data;
+} */
+
+
+/* Guardar localStorage */
+let saveFavoritesData = (key, data) => {
+    window.localStorage.setItem(key, JSON.stringify(data));
 }
 
 /* Botón 'Mostrar más */
@@ -19,14 +57,14 @@ const showMore = (response, actualPage) => {
         showMoreButton.remove();
         if (actualPage === 42) {
             previousPageButtonClick(actualPage)
-            buildCards(5, response);
+            buildCards(5, response, actualPage);
         } else {
             if ((actualPage - 1) === 0) nextPageButtonClick(actualPage);
             else {
                 previousPageButtonClick(actualPage);
                 nextPageButtonClick(actualPage);
             }
-            buildCards(19, response);
+            buildCards(19, response, actualPage);
         }
     })
 }
@@ -83,9 +121,9 @@ const modalWindow = (response) => {
     Array.from(modalButtons).map((element, index) => {
         modalButtons[index].addEventListener('click', () => {
             modalContainer.classList.add('show-modal');
-            modalImage.style.backgroundImage = `url(${response.data.results[index].image})`;
+            modalImage.style.backgroundImage = `url(${response[index].image})`;
             modalImage.style.backgroundSize = 'cover';
-            modalText.textContent = response.data.results[index].name;
+            modalText.textContent = response[index].name;
         });
         closeButton.addEventListener('click', function () {
             modalContainer.classList.remove('show-modal');
@@ -94,7 +132,7 @@ const modalWindow = (response) => {
 }
 
 /* Crea los Cards necesarios */
-const buildCards = (newCards, response) => {
+const buildCards = (newCards, response, actualPage) => {
     const container = document.getElementsByClassName('grid-container')[0];
     const cards = document.getElementsByClassName('card');
     document.getElementById('trigger').setAttribute('class', 'trigger');
@@ -114,11 +152,21 @@ const buildCards = (newCards, response) => {
 
     /* Da los valores correspondientes a cada Card */
     Array.from(cards).map((element, index) => {
-        document.getElementsByClassName('item-0')[index].style.backgroundImage = `url(${response.data.results[index].image})`;
-        document.getElementsByClassName('item-1')[index].textContent = response.data.results[index].gender;
-        document.getElementsByClassName('item-2')[index].textContent = response.data.results[index].species;
-        document.getElementsByClassName('item-3')[index].textContent = response.data.results[index].name;
-        document.getElementsByClassName('item-4')[index].textContent = response.data.results[index].status;
+
+        let image = document.getElementsByClassName('item-0')[index].style.backgroundImage = `url(${response[index].image})`;
+        image = image.replace(/(url\(|\)|"|')/g, '');
+        let gender = document.getElementsByClassName('item-1')[index].textContent = response[index].gender;
+        let species = document.getElementsByClassName('item-2')[index].textContent = response[index].species;
+        let name = document.getElementsByClassName('item-3')[index].textContent = response[index].name;
+        let status = document.getElementsByClassName('item-4')[index].textContent = response[index].status
+
+
+        /* Guardar en favoritos */
+        document.getElementsByClassName('item-3')[index].addEventListener('click', () => {
+            key = actualPage + '' + index;
+            let data = { 'image': image, 'name': name, 'gender': gender, 'status': status, 'species': species };
+            saveFavoritesData(key, data);
+        })
     });
     modalWindow(response);
 }
