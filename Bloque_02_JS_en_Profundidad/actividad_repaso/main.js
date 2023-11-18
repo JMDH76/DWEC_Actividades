15cb3fbde46d98c4b558d16be984cfee5cb2a8af
JMDH76 <jmdominguezhernandez@gmail.com>
2023-11-18 17:32:22 +0100

    cs repaso

--- Bloque_02_JS_en_Profundidad/actividad_repaso/main.js
+++ Bloque_02_JS_en_Profundidad/actividad_repaso/main.js
@@ -1,14 +1,17 @@
-const hideKeyboard = () => {
 
+const words = [];
+const wordsDefinitions = [];
+
+const hideKeyboard = () => {
 
     const keyboard = document.getElementById('keyboard-cont')
     const clone = keyboard.cloneNode(true);
     keyboard.remove()
     let newWord = '';
 
-    document.addEventListener('keydown', function (event) {
+    document.addEventListener('keydown', () => {
         const showKeyboardWord = 'a';        //'abracadabra';
-        const hideKeyWord = 'c'         //'patadecabra';
+        const hideKeyWord = 'c'             //'patadecabra';
         const newLetter = (event.key !== 'Meta') ? event.key.toLowerCase() : ''; // Evita el uso de la tecla 'Meta'
 
         if (newLetter !== 'enter' && newLetter !== 'backspace') {
@@ -16,7 +19,6 @@ const hideKeyboard = () => {
         } else if (newLetter === 'backspace') {
             newWord = newWord.slice(0, -1);
         }
-
         if (newWord === showKeyboardWord) {
             document.getElementsByTagName('table')[1].appendChild(clone);
             newWord = '';
@@ -25,49 +27,46 @@ const hideKeyboard = () => {
         } else if (newWord === hideKeyWord) {
             document.getElementById('keyboard-cont').remove();
             newWord = '';
+
+            const letsPlay = document.getElementsByTagName('h1')[0];
+            letsPlay.addEventListener('click', () => {
+
+                if (localStorage.length > 0) {
+                    Array.from(localStorage).map((element, index) => {
+                        let key = index + 1
+                        let word = localStorage.getItem(key);
+                        word = word.replace(/"/g, '');
+                        words.push(word);
+                        getApiData(words[index]);
+                    })
+                }
+                console.log(wordsDefinitions);
+            });
         }
     });
 }
 
 const getApiData = async (word) => {
+
     let response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    let definitionsNumber = [];
+    let meaningsNumber = (response.data[0].meanings).length;
+    let definitions = [];
 
-    let length_0 = (response.data[0].meanings).length;
-    let length_1 = (response.data[0].meanings[0].definitions).length;
-    let length_2 = (response.data[0].meanings[1].definitions).length;
-  /*   let length_3 = (response.data[1].meanings[0].definitions).length;
-    let length_4 = (response.data[1].meanings[1].definitions).length; */
-    let length_data = (response.data).length
+    const arrayMeaningsNumber = Array.from({ length: meaningsNumber }, (element, index) => index);
 
-    console.log('length_0 > ' + length_0);
-    console.log('length_1 > ' + length_1);
-    console.log('length_2 > ' + length_2);
-/*     console.log('length_3 > ' + length_3);
-    console.log('length_4 > ' + length_4); */
-
-    console.log('length_data > ' + length_data);
-
-
-    for (let i = 0; i < length_0; i++) {
-        if (i === 0) {
-            for (let h = 0; h < length_1; h++) {
-                console.log(response.data[1].meanings[i].definitions[h].definition);
-            }
-        } else if (i === 1) {
-            for (let j = 0; j < length_2; j++) {
-                console.log(response.data[1].meanings[i].definitions[j].definition);
-            }
-        } else if (i === 2) {
-            for (let k = 0; k < length_2; k++) {
-                console.log('k > ' + response.data[1].meanings[i].definitions[k].definition);
-            }
+    arrayMeaningsNumber.map((element, index) => {
+        let length_meanings = (response.data[0].meanings[index].definitions).length;
+        definitionsNumber.push(length_meanings);
+        for (let j = 0; j < definitionsNumber[index]; j++) {
+            let definition = response.data[0].meanings[index].definitions[j].definition;
+            definitions.push(definition);
         }
-    }
+    })
+    wordsDefinitions.push(definitions);
 }
 
-
-getApiData('love');
-
+//getApiData('pace');
 
 const addWord = () => {
     let typedLetters = '';
@@ -80,10 +79,8 @@ const addWord = () => {
             if (value === 'submit') {
                 saveData(localStorage.length + 1, typedLetters)
                 typedLetters = '';
-
             } else if (value === 'del') {
                 typedLetters = typedLetters.slice(0, -1);
-
             } else {
                 typedLetters += value;
             }
