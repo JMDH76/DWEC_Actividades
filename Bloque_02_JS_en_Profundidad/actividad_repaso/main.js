f2b41a4d418062e4ad3804267cfce0a638d18a00
JMDH76 <jmdominguezhernandez@gmail.com>
2023-11-19 00:12:09 +0100

    cs repaso

--- Bloque_02_JS_en_Profundidad/actividad_repaso/main.js
+++ Bloque_02_JS_en_Profundidad/actividad_repaso/main.js
@@ -1,9 +1,11 @@
-
 const words = [];
 const wordsDefinitions = [];
+let counter = 3;
+let flag = true;
+let gameWords = words.length;
+let rigthWords = 0;
 
 const hideKeyboard = () => {
-
     const keyboard = document.getElementById('keyboard-cont')
     const clone = keyboard.cloneNode(true);
     keyboard.remove()
@@ -29,32 +31,42 @@ const hideKeyboard = () => {
             newWord = '';
 
             const letsPlay = document.getElementsByTagName('h1')[0];
-            letsPlay.addEventListener('click', () => {
 
+            letsPlay.addEventListener('click', () => {
                 if (localStorage.length > 0) {
                     Array.from(localStorage).map((element, index) => {
-                        let key = index + 1
+                        let key = index + 1;
                         let word = localStorage.getItem(key);
                         word = word.replace(/"/g, '');
                         words.push(word);
-                        getApiData(words[index]);
-                    })
+                        getApiData(words[index], index);
+                    });
                 }
-                console.log(wordsDefinitions);
             });
         }
     });
 }
 
-const getApiData = async (word) => {
+
+
+const obtenerDatos = () => {
+    //console.log(words[counter]);
+    const long = wordsDefinitions[counter].length;
+    console.log(long)
+    for (let i = 0; i < long; i++) {
+        console.log(wordsDefinitions[counter][i])
+    }
+}
+
+
+const getApiData = async (word, index) => {
 
     let response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     let definitionsNumber = [];
-    let meaningsNumber = (response.data[0].meanings).length;
+    let meaningsLength = (response.data[0].meanings).length;
     let definitions = [];
 
-    const arrayMeaningsNumber = Array.from({ length: meaningsNumber }, (element, index) => index);
-
+    let arrayMeaningsNumber = Array.from({ length: meaningsLength }, (element, index) => index);
     arrayMeaningsNumber.map((element, index) => {
         let length_meanings = (response.data[0].meanings[index].definitions).length;
         definitionsNumber.push(length_meanings);
@@ -62,11 +74,68 @@ const getApiData = async (word) => {
             let definition = response.data[0].meanings[index].definitions[j].definition;
             definitions.push(definition);
         }
-    })
+    });
     wordsDefinitions.push(definitions);
+
+    if (flag === true) {
+
+        obtenerDatos();
+        let word = words[counter];
+        let wordLength = word.length;
+        let tr = document.getElementsByTagName('tr')[0];
+        let casillas = document.getElementsByClassName('activity');
+
+        Array.from(casillas).map((element, index) => {
+            if (index > 0) element.remove();
+        })
+
+        let arrayWordLength = Array.from({ length: wordLength - 1 }, (element, index) => index);
+        arrayWordLength.map((element, index) => {
+            let casilla = casillas[index].cloneNode(true);
+            tr.appendChild(casilla);
+        })
+
+        let celdaAnterior = undefined;
+        let w = word.split("");
+
+        Array.from(casillas).map((element, index) => {
+            const randomIndex = Math.floor(Math.random() * w.length);
+            element.innerHTML = w[randomIndex];
+            w.splice(randomIndex, 1);
+
+
+            element.addEventListener('click', () => {
+                if (celdaAnterior !== undefined) {
+                    let temp = element.innerHTML;
+                    element.innerHTML = celdaAnterior.innerHTML;
+                    celdaAnterior.innerHTML = temp;
+                    celdaAnterior = undefined;
+                } else {
+                    celdaAnterior = element;
+                }
+
+                let provisionalWord = '';
+                for (let i = 0; i < word.length; i++) {
+                    let letter = document.getElementsByClassName('activity')[i].innerHTML;
+                    provisionalWord += letter;
+                }
+                console.log(provisionalWord);
+
+                if (provisionalWord === word) {
+                    console.log('acierto');
+                    flag = true;
+                    rigthWords++;
+                    counter++
+                } else flag = false;
+
+            })
+        })
+
+        //counter++;
+        //console.log(counter);
+    }
 }
 
-//getApiData('pace');
 
 const addWord = () => {
     let typedLetters = '';
